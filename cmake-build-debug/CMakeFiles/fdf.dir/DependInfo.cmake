7459aa6c7d8f052cd82da509130ff834380b30cb
tnichell <56081527+tnichell@users.noreply.github.com>
2020-02-03 17:39:24 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/fdf.dir/DependInfo.cmake
@@ -0,0 +1,91 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/tnichell/fdf/draw.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/draw.c.o"
+  "/Users/tnichell/fdf/fdf.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/fdf.c.o"
+  "/Users/tnichell/fdf/libft/ft_atoi.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_atoi.c.o"
+  "/Users/tnichell/fdf/libft/ft_bzero.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_bzero.c.o"
+  "/Users/tnichell/fdf/libft/ft_degree.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_degree.c.o"
+  "/Users/tnichell/fdf/libft/ft_isalnum.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_isalnum.c.o"
+  "/Users/tnichell/fdf/libft/ft_isalpha.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_isalpha.c.o"
+  "/Users/tnichell/fdf/libft/ft_isascii.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_isascii.c.o"
+  "/Users/tnichell/fdf/libft/ft_isdigit.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_isdigit.c.o"
+  "/Users/tnichell/fdf/libft/ft_isprint.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_isprint.c.o"
+  "/Users/tnichell/fdf/libft/ft_itoa.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_itoa.c.o"
+  "/Users/tnichell/fdf/libft/ft_lstadd.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_lstadd.c.o"
+  "/Users/tnichell/fdf/libft/ft_lstdel.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_lstdel.c.o"
+  "/Users/tnichell/fdf/libft/ft_lstdelone.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_lstdelone.c.o"
+  "/Users/tnichell/fdf/libft/ft_lstiter.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_lstiter.c.o"
+  "/Users/tnichell/fdf/libft/ft_lstmap.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_lstmap.c.o"
+  "/Users/tnichell/fdf/libft/ft_lstnew.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_lstnew.c.o"
+  "/Users/tnichell/fdf/libft/ft_memalloc.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_memalloc.c.o"
+  "/Users/tnichell/fdf/libft/ft_memccpy.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_memccpy.c.o"
+  "/Users/tnichell/fdf/libft/ft_memchr.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_memchr.c.o"
+  "/Users/tnichell/fdf/libft/ft_memcmp.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_memcmp.c.o"
+  "/Users/tnichell/fdf/libft/ft_memcpy.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_memcpy.c.o"
+  "/Users/tnichell/fdf/libft/ft_memdel.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_memdel.c.o"
+  "/Users/tnichell/fdf/libft/ft_memmove.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_memmove.c.o"
+  "/Users/tnichell/fdf/libft/ft_memset.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_memset.c.o"
+  "/Users/tnichell/fdf/libft/ft_mod.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_mod.c.o"
+  "/Users/tnichell/fdf/libft/ft_putchar.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_putchar.c.o"
+  "/Users/tnichell/fdf/libft/ft_putchar_fd.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_putchar_fd.c.o"
+  "/Users/tnichell/fdf/libft/ft_putendl.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_putendl.c.o"
+  "/Users/tnichell/fdf/libft/ft_putendl_fd.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_putendl_fd.c.o"
+  "/Users/tnichell/fdf/libft/ft_putnbr.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_putnbr.c.o"
+  "/Users/tnichell/fdf/libft/ft_putnbr_fd.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_putnbr_fd.c.o"
+  "/Users/tnichell/fdf/libft/ft_putstr.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_putstr.c.o"
+  "/Users/tnichell/fdf/libft/ft_putstr_fd.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_putstr_fd.c.o"
+  "/Users/tnichell/fdf/libft/ft_root.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_root.c.o"
+  "/Users/tnichell/fdf/libft/ft_splitt.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_splitt.c.o"
+  "/Users/tnichell/fdf/libft/ft_strcat.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strcat.c.o"
+  "/Users/tnichell/fdf/libft/ft_strchr.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strchr.c.o"
+  "/Users/tnichell/fdf/libft/ft_strclr.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strclr.c.o"
+  "/Users/tnichell/fdf/libft/ft_strcmp.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strcmp.c.o"
+  "/Users/tnichell/fdf/libft/ft_strcpy.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strcpy.c.o"
+  "/Users/tnichell/fdf/libft/ft_strdel.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strdel.c.o"
+  "/Users/tnichell/fdf/libft/ft_strdup.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strdup.c.o"
+  "/Users/tnichell/fdf/libft/ft_strequ.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strequ.c.o"
+  "/Users/tnichell/fdf/libft/ft_striter.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_striter.c.o"
+  "/Users/tnichell/fdf/libft/ft_striteri.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_striteri.c.o"
+  "/Users/tnichell/fdf/libft/ft_strjoin.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strjoin.c.o"
+  "/Users/tnichell/fdf/libft/ft_strjoin2.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strjoin2.c.o"
+  "/Users/tnichell/fdf/libft/ft_strlcat.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strlcat.c.o"
+  "/Users/tnichell/fdf/libft/ft_strlen.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strlen.c.o"
+  "/Users/tnichell/fdf/libft/ft_strlen2.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strlen2.c.o"
+  "/Users/tnichell/fdf/libft/ft_strmap.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strmap.c.o"
+  "/Users/tnichell/fdf/libft/ft_strmapi.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strmapi.c.o"
+  "/Users/tnichell/fdf/libft/ft_strncat.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strncat.c.o"
+  "/Users/tnichell/fdf/libft/ft_strncmp.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strncmp.c.o"
+  "/Users/tnichell/fdf/libft/ft_strncpy.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strncpy.c.o"
+  "/Users/tnichell/fdf/libft/ft_strnequ.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strnequ.c.o"
+  "/Users/tnichell/fdf/libft/ft_strnew.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strnew.c.o"
+  "/Users/tnichell/fdf/libft/ft_strnstr.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strnstr.c.o"
+  "/Users/tnichell/fdf/libft/ft_strrchr.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strrchr.c.o"
+  "/Users/tnichell/fdf/libft/ft_strsplit.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strsplit.c.o"
+  "/Users/tnichell/fdf/libft/ft_strstr.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strstr.c.o"
+  "/Users/tnichell/fdf/libft/ft_strsub.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strsub.c.o"
+  "/Users/tnichell/fdf/libft/ft_strtrim.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_strtrim.c.o"
+  "/Users/tnichell/fdf/libft/ft_tolower.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_tolower.c.o"
+  "/Users/tnichell/fdf/libft/ft_toupper.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_toupper.c.o"
+  "/Users/tnichell/fdf/libft/ft_unletter.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/ft_unletter.c.o"
+  "/Users/tnichell/fdf/libft/get_next_line.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/libft/get_next_line.c.o"
+  "/Users/tnichell/fdf/main.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/main.c.o"
+  "/Users/tnichell/fdf/read_file.c" "/Users/tnichell/fdf/cmake-build-debug/CMakeFiles/fdf.dir/read_file.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../libft"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
